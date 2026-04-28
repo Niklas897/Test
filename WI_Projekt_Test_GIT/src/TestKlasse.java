4595af3a801f4dc5439fab79bdc31545096a447f
52hani1bwi <52hani1bwi@c-usw04.ad.hft-stuttgart.de>
2026-04-28 07:57:35 +0200

    Second Change

--- WI_Projekt_Test_GIT/src/TestKlasse.java
+++ WI_Projekt_Test_GIT/src/TestKlasse.java
@@ -4,6 +4,7 @@ public class TestKlasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bernd das Brot ist toll!");
+		System.out.println("Tim is schwul!");
 	}
 
 }
